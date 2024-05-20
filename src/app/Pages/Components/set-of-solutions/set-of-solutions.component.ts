247315941505847a0bf1c8d5203e37c59b0be33e
AKalityuk <AKalityuk@nota.tech>
2024-05-20 13:40:04 +0300

    set-of-solutions

--- src/app/Pages/Components/set-of-solutions/set-of-solutions.component.ts
+++ src/app/Pages/Components/set-of-solutions/set-of-solutions.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-set-of-solutions',
   templateUrl: './set-of-solutions.component.html',
-  styleUrl: './set-of-solutions.component.scss'
+  styleUrl: './set-of-solutions.component.scss',
 })
 export class SetOfSolutionsComponent {
-
+  set = [
+    {
+      title: 'WI-FI аудиобейджи',
+      img: './assets/wifi.png',
+      color: 'light-blue',
+    },
+    {
+      title: 'WI-FI микрофоны',
+      img: './assets/micro.png',
+      color: 'light-green',
+    },
+    {
+      title: 'Bluetooth наушники',
+      img: './assets/headphones.png',
+      color: 'light-orange',
+    },
+    {
+      title: 'Приложения для смартфонов и планшетов',
+      img: './assets/iphone.png',
+      color: 'light-green',
+    },
+    {
+      title: 'Клиентские системы и устройства записи',
+      img: './assets/monitor.png',
+      color: 'light-blue',
+    },
+  ];
 }
